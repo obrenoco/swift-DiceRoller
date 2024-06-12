209e086b679f3c7364bbebf7b5f30070c63d6393
Breno <bsromeiro@gmail.com>
2024-06-12 18:15:11 -0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Breno Romeiro on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
